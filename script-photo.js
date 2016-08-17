3a11ef3e9629217143a716054b3e42e5ddfcfb04
Artem Savienkov <savienkov.ao@gmail.com>
2016-08-17 21:45:24 +0300

    JS changes

--- script-photo.js
+++ script-photo.js
@@ -1,30 +1,50 @@
 "use strict";
-// alert('Hello!');
-/*
-var galleryPhoto = document.querySelectorAll('.content-image')[0];
-
-galleryPhoto.onclick = function(){
-	var newDiv = document.createElement('div');
-	newDiv.style.width = '600px';
-	newDiv.style.height = '600px';
-	newDiv.style.backgroundImage = 'url(images/ogasaVL/ogasaVL_(1).jpg;';
-	newDiv.style.position = 'fixed';
-	document.body.appendChild(newDiv);
-}
-*/
 
 var btnOpenMore = document.querySelectorAll('.btn_open_more')[0];
 var galleryHidden =  document.querySelectorAll('.gallery_hidden')[0];
 var galleryPhoto = document.querySelectorAll('.content-image');
+var currentScreenWidth = document.documentElement.clientWidth;
+var galleryPhotoStep = galleryPhoto[0].offsetHeight*5;
+galleryHidden.style.height = galleryPhotoStep +'px';
+galleryHidden.style.overflow = 'hidden';
+console.log(galleryPhotoStep);
+var sum = 1;
 
 btnOpenMore.onclick = function(){
-	var galleryHiddenHeight = parseInt(galleryHidden.style.height)||100;
-	var heightGrowth = galleryHiddenHeight+100;
-	galleryHidden.style.height = heightGrowth + 'em';
-	
-	if (heightGrowth>=(Math.ceil(((galleryPhoto.length+1)*100)/15))){
-		galleryHidden.style.height = (Math.ceil((((galleryPhoto.length+1)*100)/15)/100)*100) +'em';
-		console.log(galleryHidden.style.height);
-	}
-	console.log(galleryHidden.style.height);
+	abr();
 }
+	
+function abr(){
+	sum++;
+	console.log(sum);
+	if (currentScreenWidth<=480){
+		if (getGalleryHeigth()>= (Math.ceil(((galleryPhoto.length+1)*galleryPhotoStep)/5))){
+			galleryHidden.style.height = (Math.ceil((((galleryPhoto.length+1)*galleryPhotoStep)/5)/100)*100) +'px';
+		}
+	} else {
+		if (getGalleryHeigth()>= (Math.ceil(((galleryPhoto.length+1)*galleryPhotoStep)/20))){
+			galleryHidden.style.height = (Math.ceil((((galleryPhoto.length+1)*galleryPhotoStep)/20)/100)*100) +'px';
+			return;
+		}
+	}
+	
+}
+
+function getGalleryHeigth (){
+	var galleryHiddenHeight = parseInt(galleryHidden.style.height)||galleryPhotoStep;
+	var heightGrowth = galleryHiddenHeight+galleryPhotoStep;
+	galleryHidden.style.height = heightGrowth + 'px';
+	return heightGrowth;
+}
+
+
+window.onresize=function(){
+	if (currentScreenWidth<=480){
+		galleryPhotoStep = galleryPhoto[0].offsetHeight*20;
+		galleryHidden.style.height = galleryPhotoStep*sum +'px';
+		console.log('!')
+	} else {
+		galleryPhotoStep = galleryPhoto[0].offsetHeight*5;
+		galleryHidden.style.height = galleryPhotoStep*sum +'px';
+	}
+};
