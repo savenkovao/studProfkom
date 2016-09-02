d0c42dfaf4917c21e868653fe0ff3c83140850ae
Artem Savienkov <savienkov.ao@gmail.com>
2016-09-02 17:53:29 +0300

    JS changes
    'open photo' script

--- script-open_photo.js
+++ script-open_photo.js
@@ -6,6 +6,10 @@ var body = document.getElementsByTagName('body')[0];
 var newDiv;
 var clval;
 var cloneNode;
+var imageContainer;
+var currentWrapperWidth = window.innerWidth;
+var currentWrapperHeight = window.innerHeight;
+var cloneNodeWidth;
 
 (function () {
     var  len = photoLink.length;
@@ -19,20 +23,25 @@ var cloneNode;
 			newDiv.style.width = '100%';
 			newDiv.style.height = '100%';
 			newDiv.style.top = '0%';
-			newDiv.style.padding = '0 10vw';
-			newDiv.style.display = 'flex';
-			newDiv.style.flexDirection = 'column';
-			newDiv.style.justifyContent = 'center';
-			newDiv.style.alignItems = 'center';
-			newDiv.style.backgroundColor = 'rgba(0,0,0,0.8)';
 			newDiv.style.zIndex = '10';
 
+			imageContainer = document.createElement('div');
+			imageContainer.style.position = 'relative';
+			imageContainer.style.width = '100%';
+			imageContainer.style.height = '100%';
+			imageContainer.style.padding = '0 10vw';
+			imageContainer.style.display = 'flex';
+			imageContainer.style.flexDirection = 'column';
+			imageContainer.style.justifyContent = 'center';
+			imageContainer.style.alignItems = 'center';
+			imageContainer.style.backgroundColor = 'rgba(0,0,0,0.9)';
+		
 			var btnRight = document.createElement('div');
 			btnRight.style.display = 'block';
-			btnRight.style.width = '10%';
+			btnRight.style.width = '30%';
 			btnRight.style.height = '100%';
 			btnRight.style.position = 'absolute';
-			btnRight.style.right = '0';
+			btnRight.style.left = '70%';
 			btnRight.style.top = '0';
 			btnRight.style.backgroundColor = 'transparent';
 			btnRight.style.color = 'white';
@@ -45,15 +54,15 @@ var cloneNode;
 			arrowRight.style.position = 'absolute';
 			arrowRight.style.width = '20px';
 			arrowRight.style.height = '50px';
-			arrowRight.style.right = 'calc(50% - 10px)';
+			arrowRight.style.right = '10%';
 			arrowRight.style.top = 'calc(50% - 25px)';
+			arrowRight.style.opacity = '0.05';
+			arrowRight.style.backgroundImage = 'url(images/slider_arrow.png)';
 			btnRight.appendChild(arrowRight);
-
-
 
 			var btnLeft = document.createElement('div');
 			btnLeft.style.display = 'block';
-			btnLeft.style.width = '10%';
+			btnLeft.style.width = '30%';
 			btnLeft.style.height = '100%';
 			btnLeft.style.position = 'absolute';
 			btnLeft.style.left = '0';
@@ -66,28 +75,63 @@ var cloneNode;
 			arrowLeft.style.position = 'absolute';
 			arrowLeft.style.width = '20px';
 			arrowLeft.style.height = '50px';
-			arrowLeft.style.left = 'calc(50% - 10px)';
+			arrowLeft.style.left = '10%';
 			arrowLeft.style.top = 'calc(50% - 25px)';
 			arrowLeft.style.transform = 'rotate(180deg)';
+			arrowLeft.style.opacity = '0.05';
+			arrowLeft.style.backgroundImage = 'url(images/slider_arrow.png)';
 			btnLeft.appendChild(arrowLeft);
-			
+
+			var btnClose = document.createElement('div');
+			btnClose.style.display = 'block';
+			btnClose.style.width = '70px';
+			btnClose.style.height = '70px';
+			btnClose.style.position = 'absolute';
+			btnClose.style.right = '0';
+			btnClose.style.top = '0';
+			btnClose.style.backgroundColor = 'transparent';
+
+			var closePic = document.createElement('div');
+			closePic.style.backgroundPosition = 'center';
+			closePic.style.backgroundSize = 'cover';
+			closePic.style.position = 'absolute';
+			closePic.style.width = '25px';
+			closePic.style.height = '25px';
+			closePic.style.right = 'calc(50% - 12.5px)';
+			closePic.style.top = 'calc(50% - 12.5px)';
+			closePic.style.opacity = '0.3';
+			closePic.style.backgroundImage = 'url(images/close_button.png)';
+			btnClose.appendChild(closePic);
 
 			addImage ();
 
 			function addImage (){
-				cloneNode = photoInLink[e].cloneNode(true);
-				cloneNode.style.height = '97%';
-				cloneNode.style.width = 'auto';
+				cloneNode = photoInLink[e].cloneNode(true);		
+				getImageSize ();
 				cloneNode.style.padding = ' 0 ';
 				cloneNode.style.border = '3px solid rgba(255,255,255,0.7)';
 				cloneNode.style.borderRadius = '0.5vw';
-				newDiv.appendChild(cloneNode);
-			}			
+				imageContainer.appendChild(cloneNode);
 
-			var currentWrapperWidth = document.body.offsetWidth;
-						
-			newDiv.appendChild(btnRight);
-			newDiv.appendChild(btnLeft);
+			}
+
+			function getImageSize (){
+				if((currentWrapperWidth/currentWrapperHeight)<(photoInLink[e].offsetWidth/photoInLink[e].offsetHeight)){
+					cloneNode.style.width = '100%';
+					cloneNode.style.height = 'auto';
+					cloneNodeWidth = currentWrapperWidth;
+
+				} else {
+					cloneNode.style.width = 'auto';
+					cloneNode.style.height = '97%';
+					cloneNodeWidth = (currentWrapperHeight*0.97) * photoInLink[e].offsetWidth/photoInLink[e].offsetHeight;
+				}	
+			}
+		
+			imageContainer.appendChild(btnRight);
+			imageContainer.appendChild(btnLeft);
+			imageContainer.appendChild(btnClose);
+			newDiv.appendChild(imageContainer);
 
 			btnRight.onclick = function(event){
 				event.stopPropagation();
@@ -110,42 +154,50 @@ var cloneNode;
 				addImage ();	
 			}
 
-
 		btnLeft.addEventListener("mouseover", changeBackgroundBtnLeft);	
 		btnLeft.addEventListener("mouseout",deleteBackGround);
 		btnRight.addEventListener("mouseover", changeBackgroundBtnRight);	
 		btnRight.addEventListener("mouseout",deleteBackGround);
+		btnClose.addEventListener("mouseover", changeBackgroundBtnClose);	
+		btnClose.addEventListener("mouseout",deleteBackGround);
 
 			function changeBackgroundBtnLeft (){
-				btnLeft.style.backgroundColor = 'rgba(255,255,255,0.1)';
-				arrowLeft.style.backgroundImage = 'url(images/slider_arrow.png)';
+				// btnLeft.style.backgroundColor = 'rgba(255,255,255,0.1)';				
+				arrowLeft.style.opacity = '1';
 			}
 
 			function changeBackgroundBtnRight (){
-				btnRight.style.backgroundColor = 'rgba(255,255,255,0.1)';
-				arrowRight.style.backgroundImage = 'url(images/slider_arrow.png)';
+				// btnRight.style.backgroundColor = 'rgba(255,255,255,0.1)';			
+				arrowRight.style.opacity = '1';
+			}
+
+			function changeBackgroundBtnClose () {
+				// btnClose.style.backgroundColor ='red';
+				closePic.style.opacity = '0.8';
 			}
 
 			function deleteBackGround (){
 				btnLeft.style.backgroundColor = 'transparent';
 				btnRight.style.backgroundColor = 'transparent';
-				arrowRight.style.backgroundImage = 'none';
-				arrowLeft.style.backgroundImage = 'none';
+				arrowRight.style.opacity = '0.4';
+				arrowLeft.style.opacity = '0.4';
+				closePic.style.opacity = '0.3';
 			}
-
-
-
-
 
 			body.appendChild(newDiv);
 		
-
 			newDiv.onclick = function (){
 				newDiv.remove();
 			}
 
 			cloneNode.onclick = function (){
 				newDiv.remove();
+			}
+
+			window.onresize=function(){
+				currentWrapperWidth = window.innerWidth;
+				currentWrapperHeight = window.innerHeight;
+				getImageSize ();
 			}
         };
 
